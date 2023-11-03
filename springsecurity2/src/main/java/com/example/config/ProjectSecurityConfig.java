7ac9f59bcb4726aa6bc4cff13b1b7744ea14961e
vakser <65089446+vakser@users.noreply.github.com>
2023-11-03 16:28:30 +0200

    Project security configuration added and app breakpoints separated in 2 groups due to authentication needed or not.

---
+++ springsecurity2/src/main/java/com/example/config/ProjectSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .headers(Customizer.withDefaults());
+        return http.build();
+    }
+}
