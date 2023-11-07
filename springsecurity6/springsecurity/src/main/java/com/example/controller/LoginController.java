5fe26338085794766aed4df0d32e91085591a8af
vakser <65089446+vakser@users.noreply.github.com>
2023-11-07 21:27:36 +0200

    EazyBank fullstack project (frontend - Angular, backend - Spring Boot) set up.

---
+++ springsecurity6/springsecurity/src/main/java/com/example/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.example.controller;
+
+import com.example.model.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+        Customer savedCustomer;
+        ResponseEntity<String> response = null;
+        try {
+            String hashPassword = passwordEncoder.encode(customer.getPassword());
+            customer.setPassword(hashPassword);
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occured due to " + ex.getMessage());
+        }
+        return response;
+    }
+}
