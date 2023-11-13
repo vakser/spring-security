d1b7c27e49b1432655fafb908644d9b445a22e81
vakser <65089446+vakser@users.noreply.github.com>
2023-11-13 14:55:39 +0200

    New module added to the project, backend submodule updated to use method level security with @PreAuthorize.

--- springsecurity10/springsecurity10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecurity10/springsecurity10/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -2,9 +2,11 @@ package com.eazybytes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
 public class EazyBankBackendApplication {
 
 	public static void main(String[] args) {
