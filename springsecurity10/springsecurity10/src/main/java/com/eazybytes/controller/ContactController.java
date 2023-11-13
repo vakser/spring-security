3934eff2e59be7023be55af8db74fa17eccda52d
vakser <65089446+vakser@users.noreply.github.com>
2023-11-13 16:25:26 +0200

    Method level security using @PreFilter implemented. Frontend submodule added and updated to use method level security.

--- springsecurity10/springsecurity10/src/main/java/com/eazybytes/controller/ContactController.java
+++ springsecurity10/springsecurity10/src/main/java/com/eazybytes/controller/ContactController.java
@@ -1,9 +1,12 @@
 package com.eazybytes.controller;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,15 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    @PreFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+        List<Contact> returnContacts = new ArrayList<>();
+        returnContacts.add(contact);
+        return returnContacts;
     }
 
     public String getServiceReqNumber() {
