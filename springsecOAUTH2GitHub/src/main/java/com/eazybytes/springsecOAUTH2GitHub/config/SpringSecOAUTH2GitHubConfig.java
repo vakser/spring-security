0acc4df0711f727bb20c477c4fc5434440bb8565
vakser <65089446+vakser@users.noreply.github.com>
2023-11-14 12:21:12 +0200

    New module added - Spring Boot application that uses GitHub Auth server for authentication of the user.

---
+++ springsecOAUTH2GitHub/src/main/java/com/eazybytes/springsecOAUTH2GitHub/config/SpringSecOAUTH2GitHubConfig.java
@@ -0,0 +1,34 @@
+package com.eazybytes.springsecOAUTH2GitHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTH2GitHubConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)->requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+    /*@Bean
+    public ClientRegistrationRepository clientRepository() {
+        ClientRegistration clientReg = clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientReg);
+    }
+
+    private ClientRegistration clientRegistration() {
+		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("2c26c6cf400f88721c73")
+	           .clientSecret("b32b43679866bf42d89e420954868a60f0fc72dc").build();
+    }*/
+
+}
